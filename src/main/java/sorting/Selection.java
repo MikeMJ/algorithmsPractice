477a2f17dab27ce5d933460643a475cc251cc08a
mabdullayev <mabdullayev@azercell.com>
2017-12-27 14:09:39 +0400

    Added Selection sort trace as a table of changes.

--- src/main/java/sorting/Selection.java
+++ src/main/java/sorting/Selection.java
@@ -1,15 +1,29 @@
 package sorting;
 
+import edu.princeton.cs.introcs.StdDraw;
+
+
+import java.awt.*;
+
 public class Selection {
-    public static void sort(Comparable[] array) {
+
+    private static final int ARRAY_ITEM_WIDTH = 10;
+
+    public static void sort(Comparable[] array, boolean trace) {
+        if (trace)
+            createTraceCap(array);
         int N = array.length;
         for (int i = 0; i < N; i++) {
+
             int min = i;
             for (int j = i + 1; j < N; j++) {
-                if (less(array[j],array[min]))
+                if (less(array[j], array[min]))
                     min = j;
             }
+            if (trace)
+                trace(array, i, min);
             exchange(array, i, min);
+
         }
     }
 
@@ -30,9 +44,47 @@ public class Selection {
         }
     }
 
+    private static void createTraceCap(Object[] array) {
+        int N = array.length;
+        int maxY = ARRAY_ITEM_WIDTH + N * ARRAY_ITEM_WIDTH, maxX = maxY;
+        StdDraw.setPenColor(Color.black);
+        Font font = new Font("Consolas", Font.BOLD, 14);
+        StdDraw.setFont(font);
+        StdDraw.setScale(0, maxY);
+        StdDraw.text(0, maxY, "i");
+        StdDraw.text(ARRAY_ITEM_WIDTH, maxY, "min");
+        for (int i = 0; i < N; i++) {
+            StdDraw.text(ARRAY_ITEM_WIDTH*2 + i * ARRAY_ITEM_WIDTH, maxY, String.valueOf(i));
+            if (i == 0) {
+                StdDraw.setPenColor(Color.RED);
+                StdDraw.line(-ARRAY_ITEM_WIDTH/2, maxY - ARRAY_ITEM_WIDTH/2, maxX+ARRAY_ITEM_WIDTH/2, maxY - ARRAY_ITEM_WIDTH/2);
+                StdDraw.setPenColor(Color.black);
+            }
+            StdDraw.text(ARRAY_ITEM_WIDTH*2 + i * ARRAY_ITEM_WIDTH, maxY - ARRAY_ITEM_WIDTH, array[i].toString());
+        }
+
+    }
+
+    private static void trace(Object[] array, int i, int min) {
+        int N = array.length;
+        int maxY = ARRAY_ITEM_WIDTH*2 + N * ARRAY_ITEM_WIDTH, maxX = maxY;
+        StdDraw.setPenColor(Color.black);
+        StdDraw.text(0, maxY - ARRAY_ITEM_WIDTH*3 - i * ARRAY_ITEM_WIDTH, String.valueOf(i));
+        StdDraw.text(ARRAY_ITEM_WIDTH, maxY - ARRAY_ITEM_WIDTH*3 - i * ARRAY_ITEM_WIDTH, String.valueOf(min));
+        StdDraw.setPenColor(196, 197, 198);
+        for (int j = 0; j < N; j++) {
+            if (j >= i)
+                StdDraw.setPenColor(Color.black);
+            if (j == min)
+                StdDraw.setPenColor(Color.red);
+            StdDraw.text(ARRAY_ITEM_WIDTH*2 + j * ARRAY_ITEM_WIDTH, maxY - ARRAY_ITEM_WIDTH*3 - i * ARRAY_ITEM_WIDTH, array[j].toString());
+        }
+
+    }
+
     public static void main(String[] args) {
-        Integer[] array = {1, 3, 5, 5, 4, 2, 7, 3, 8, 1, 9, 5, 4, 9, 3, 1, 2};
-        sort(array);
+        Character[] array = {'E', 'A', 'S', 'Y', 'Q', 'U', 'E', 'S', 'T', 'I', 'O','N'};
+        sort(array,true);
         show(array);
     }
 }
